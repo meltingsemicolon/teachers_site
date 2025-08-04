1a2ccab953221887854400be71da4d2791a7fa70
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-05 01:00:13 +0545

    Home the Hero section

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center p-4">
+        <h2 className="text-4xl font-bold text-blue-600 mb-4">Welcome to the Teachers Website</h2>
+        <p className="text-lg text-gray-700">AS we know this section has to be build further</p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
