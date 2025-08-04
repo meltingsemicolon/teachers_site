b57b0743ec1ec25115c0c9591329423d95dea7de
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-05 00:58:29 +0545

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
-import Navbar from './components/Navbar.jsx'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Teachers from "./pages/Teachers";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
     <>
-    <Navbar />
-      <div id='container'>
-        <h1>Teachers website </h1>
-      </div>
+      <Navbar />
+      <main>
+        <Home />
+        <About />
+        <Teachers />
+        <Gallery />
+        <Contact />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
