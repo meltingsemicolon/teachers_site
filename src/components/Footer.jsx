2dba8ca2530af2a2465bca9df4f0c98d28b31d1a
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-05 00:58:48 +0545

    footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white text-center py-4 mt-10">
+      <p>&copy; {new Date().getFullYear()} Teachers Portal. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
