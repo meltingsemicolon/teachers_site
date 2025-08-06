a9d9ae81666273d1b6e7f49834c761ede2a64f62
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-06 22:19:24 +0545

    cleared footer

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-blue-600 text-white text-center py-4 mt-10">
-      <p>&copy; {new Date().getFullYear()} Teachers Portal. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
