20e15999944b46a8cb67bad7b34655ce010d7077
meltingsemicolon <meltingsemicolon@gmail.com>
2025-08-07 12:23:32 +0545

    fix not showing issue

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} My Website</p>
+      <p>Follow us on social media!</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main content area.</p>
+    </div>
+  );
+}
+export default Home;
