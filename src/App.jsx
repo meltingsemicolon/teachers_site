bd6e9da1f179bbbe4936422346b9d572c27d41a1
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-08 06:57:13 +0545

    something added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar.jsx";
 
 
 function App() {
   return (
     <>
-    <h1>hello testing</h1>
+   
       <Navbar />
-      <main>
-        <Home />
-      </main>
-      <Footer />
+     
     </>
   );
 }
